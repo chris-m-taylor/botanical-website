3532bd3bbf708f3cd6351aba92b1576a21e3a70a
chris-m-taylor <chrismtaylor1998@gmail.com>
2022-02-26 23:45:34 -0500

    Using vanilla img tag for band photo.

--- components/BandImageSection/BandImageSection.js
+++ components/BandImageSection/BandImageSection.js
@@ -1,16 +1,9 @@
 import { BandImage } from "../../public/images";
-import NextImage from 'next/image'
-import { Text } from "@chakra-ui/react";
 import styles from "./styles.module.css"
-
-
 
 const BandImageSection = () => {
    return(
-      <section class={styles.container}>
-         <NextImage src={BandImage} ></NextImage>
-         <p class={styles.text}>Botanical Selections</p>
-      </section>
+         <img class={styles.img} src={BandImage.src} />
    )
 }
 
