5317612f0450efc9384242c270e15157c0dfa201
MCVGooose <vanderwegecc@g.cofc.edu>
2023-06-22 11:57:04 -0400

    fixed small img for about page

--- components/About/Bio.js
+++ components/About/Bio.js
@@ -24,7 +24,7 @@ const Bio = ({ image, summary, name }) => {
 
                <Show below="1049px">
                   <Stack className={styles.card} alignItems='center'>
-                     <img src={image.src} className={styles.imgSmall} />
+                     <img src={image} className={styles.imgSmall} />
                      <Text className={styles.name} color={"#e88fe9"}>{name}</Text>
                      <Text textAlign='center' verticalAlign='middle' className={styles.summary}>{summary}</Text>
                   </Stack>
